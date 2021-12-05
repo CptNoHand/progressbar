73c0d1b0df024185a4b74097d2ed0c56c06268f4
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-05 19:53:49 +0100

    refactor

--- html/js/script.js
+++ html/js/script.js
@@ -25,26 +25,11 @@ $('document').ready(function() {
             });
         });
     };
-
-    Progressbar.ProgressCancel = function() {
-        $("#progress-bar").stop().css( {"width": "0%", "background-color": "rgba(71, 0, 0, 0.8)"});
-        $('#progress-bar').removeClass('cancellable');        
-        $(".progress-container").fadeOut('fast', function() {
-            $("#progress-bar").css("width", 0);
-            $.post('https://progressbar/CancelAction', JSON.stringify({
-                })
-            );
-            $(".progress-button").css("display", "none");  
-        });        
-    };
     
     window.addEventListener('message', function(event) {
         switch(event.data.action) {
             case 'progress':
                 Progressbar.Progress(event.data);
-                break;
-            case 'cancel':
-                Progressbar.ProgressCancel();
                 break;
         }
     });
